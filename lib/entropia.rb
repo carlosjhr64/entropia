80fca0bfc2e8b3b7b628151e796d715b7c87f4dd
carlosjhr64 <carlosjhr64@gmail.com>
2019-12-20 11:09:41 -0800

    version bump

--- lib/entropia.rb
+++ lib/entropia.rb
@@ -1,11 +1,17 @@
 # Standard Libraries
 require 'securerandom'
+
 # Gems
 require 'base_convert'
+
 # This Gem
-require 'entropia/version'
-require 'entropia/lb'
-require 'entropia/digest'
-require 'entropia/entropia'
+module ENTROPIA
+  VERSION = '0.1.191220'
+
+  require 'entropia/lb'
+  require 'entropia/digest'
+  require 'entropia/entropia'
+end
+
 # Requires:
 #`ruby`
--- lib/entropia/version.rb
+++
@@ -1,3 +0,0 @@
-module ENTROPIA
-  VERSION = '0.0.0'
-end
