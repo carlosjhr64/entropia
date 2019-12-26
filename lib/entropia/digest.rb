f6676848dbe95707c41d2d8d6714f51f78c2d2e8
carlosjhr64 <carlosjhr64@gmail.com>
2019-12-26 15:13:35 -0800

    how much randomness can each digest hold?

--- lib/entropia/digest.rb
+++ lib/entropia/digest.rb
@@ -1,29 +1,29 @@
 module ENTROPIA
   class Entropia
-    def digest(d=Digest::SHA2.new(256))
-      string = d.hexdigest @string
-      Entropia.new(string,
+    def digest(d=Digest::SHA2.new(256), r=256)
+      Entropia.new(d.hexdigest @string,
                    base: 16,
                    # entropy becomes that in string
-                   randomness: [256.0, @randomness].min,
+                   randomness: [r, @randomness].min,
                    shuffled: true,
-                   digits: '0123456789abcdef').to_base(@base, @digits)
+                   digits: '0123456789abcdef'
+                  ).to_base(@base, @digits)
     end
 
     def sha2(n=256)
-      digest(Digest::SHA2.new(n))
+      digest(Digest::SHA2.new(n, n.to_f))
     end
 
     def sha1
-      digest(Digest::SHA1)
+      digest(Digest::SHA1, 160.0)
     end
 
     def md5
-      digest(Digest::MD5)
+      digest(Digest::MD5, 128.0)
     end
 
     def rmd160
-      digest(Digest::RMD160)
+      digest(Digest::RMD160, 160.0)
     end
   end
 end
